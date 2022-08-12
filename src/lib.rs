bca60cf5d4f600e93d6435a543668a7d42a16f84
rcsaquino <rcsaquino.md@gmail.com>
2022-08-12 14:02:24 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-//! # RsAutoGUI
+//! # rsautogui
 //!
-//! `RsAutoGUI` is a cross-platform GUI automation rust crate.
+//! `rsautogui` aims to be a cross-platform GUI automation rust crate.
 //! It lets you control the mouse and keyboard to automate interactions with other applications.
+//! Currently, it's a wrapper of multiple rust crates to have pyautogui inspired syntax in rust.
 //! 
 //! This crate takes heavy inspiration from [PyAutoGUI.](https://github.com/asweigart/pyautogui)
 
