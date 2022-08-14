dfbcb2d2f62934bb7757d13cb7947ee80e2ff6d0
rcsaquino <rcsaquino.md@gmail.com>
2022-08-14 14:59:09 +0800

    Update mod.rs

--- src/screen/mod.rs
+++ src/screen/mod.rs
@@ -6,7 +6,7 @@ use image::RgbaImage;
 use scrap::{Capturer, Display};
 use std::{thread, time};
 
-pub use image::{Rgba, ImageBuffer};
+pub use image::{ImageBuffer, Rgba};
 
 /// Returns the width and height of primary screen.
 pub fn size() -> (u16, u16) {
@@ -21,11 +21,11 @@ pub fn on_screen(x: u16, y: u16) -> bool {
 }
 
 /// Returns screenshot of the primary screen.
-pub fn screenshot(width: u16, height: u16) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
-    if !on_screen(width, height) {
-        panic!("Specified height and/or width are greater than screen size. Use screen::size() to check current screen size.")
+pub fn screenshot(x: u16, y: u16, width: u16, height: u16) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+    if !on_screen(x + width, y + height) {
+        panic!("One or more specified parameter is not within the screen size. Use screen::size() to check.")
     }
-    
+
     let mut screen = Capturer::new(Display::primary().expect("Couldn't find display"))
         .expect("Couldn't capture screen");
 
@@ -40,16 +40,22 @@ pub fn screenshot(width: u16, height: u16) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
 
     let mut img = RgbaImage::new(width.into(), height.into());
 
-    let mut c: usize = 0;
-    for y in 0..height {
-        for x in 0..width {
-            // ss is BGRA, so convert to RGBA
+    let mut pos_x = 0;
+    let mut pos_y = 0;
+    let screen_width = size().0;
+    for index_y in y..y + height {
+        for index_x in x..x + width {
+            let index: usize = (index_x as usize + (index_y as usize * screen_width as usize)) * 4;
             img.put_pixel(
-                x.into(),
-                y.into(),
-                Rgba([ss[c + 2], ss[c + 1], ss[c], ss[c + 3]]),
+                pos_x,
+                pos_y,
+                Rgba([ss[index + 2], ss[index + 1], ss[index], ss[index + 3]]),
             );
-            c += 4;
+            pos_x += 1;
+            if pos_x >= width.into() {
+                pos_y += 1;
+                pos_x = 0;
+            }
         }
     }
     return img;
@@ -65,7 +71,7 @@ pub fn printscreen(screenshot: &ImageBuffer<Rgba<u8>, Vec<u8>>, path: &str) {
 /// Locates the first pixel color similar to the one specified and returns its coordinate.
 pub fn locate_pixel(pixel: Rgba<u8>) -> Option<(u16, u16)> {
     let display = size();
-    let ss = screenshot(display.0, display.1);
+    let ss = screenshot(0, 0, display.0, display.1);
     for y in 0..display.1 {
         for x in 0..display.0 {
             if *ss.get_pixel(x.into(), y.into()) == pixel {
@@ -79,7 +85,7 @@ pub fn locate_pixel(pixel: Rgba<u8>) -> Option<(u16, u16)> {
 /// Locates all pixel colors similar to the one specified and returns their coordinates.
 pub fn locate_all_pixel(pixel: Rgba<u8>) -> Vec<(u16, u16)> {
     let display = size();
-    let ss = screenshot(display.0, display.1);
+    let ss = screenshot(0, 0, display.0, display.1);
     let mut res: Vec<(u16, u16)> = Vec::new();
     for y in 0..display.1 {
         for x in 0..display.0 {
@@ -94,7 +100,7 @@ pub fn locate_all_pixel(pixel: Rgba<u8>) -> Vec<(u16, u16)> {
 /// Get the pixel color on x, y coordinate.
 pub fn get_pixel(x: u16, y: u16) -> Rgba<u8> {
     let display = size();
-    let ss = screenshot(display.0, display.1);
+    let ss = screenshot(0, 0, display.0, display.1);
     return *ss.get_pixel(x.into(), y.into());
 }
 
@@ -106,45 +112,3 @@ pub fn get_pixel(x: u16, y: u16) -> Rgba<u8> {
 
 // Locates the first object similar to the one provided and returns its center x & y
 // fn locate_img_center() {}
-
-pub struct Screenshot {
-    data: Vec<u8>,
-    size: (u16, u16),
-}
-
-pub struct Bgra(u8, u8, u8, u8);
-
-impl Screenshot {
-    pub fn new() -> Screenshot {
-        let mut screen = Capturer::new(Display::primary().expect("Couldn't find display"))
-            .expect("Couldn't capture screen");
-        let mut ss: Vec<u8> = Vec::new();
-
-        while ss.is_empty() {
-            match screen.frame() {
-                Ok(frame) => ss = frame.to_vec(),
-                _ => thread::sleep(time::Duration::from_micros(1)),
-            };
-        }
-
-        return Screenshot {
-            data: ss,
-            size: (screen.width() as u16, screen.height() as u16),
-        };
-    }
-
-    pub fn save(&self, path: &str) {
-        let mut img = ImageBuffer::new(*&self.size.0 as u32, *&self.size.1 as u32);
-        let mut i: usize = 0;
-        for pixel in img.pixels_mut() {
-            let b = *&self.data[i];
-            let g = *&self.data[i + 1];
-            let r = *&self.data[i + 2];
-            let a = *&self.data[i + 3];
-            *pixel = image::Rgba([r, g, b, a]);
-            i += 4;
-        }
-
-        img.save(path).unwrap();
-    }
-}
