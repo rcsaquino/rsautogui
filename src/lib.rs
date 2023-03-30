eab85de4bc91e139e9ee2fd1fc2cb7d4038b0034
rcsaquino <rcsaquino.md@gmail.com>
2023-03-30 16:29:42 +0800

    more typos

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! `rsautogui` aims to be a cross-platform GUI automation rust crate.
 //! It lets you control the mouse and keyboard to automate interactions with other applications.
 //! Currently, it's a wrapper of multiple rust crates to have pyautogui inspired syntax in rust.
-//! 
+//!
 //! This crate takes heavy inspiration from [PyAutoGUI.](https://github.com/asweigart/pyautogui)
 
 pub mod keyboard;
